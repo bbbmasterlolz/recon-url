c102258eabd8e43e7860f22594723df0557952be
bbbmasterlolz <82566017+bbbmasterlolz@users.noreply.github.com>
2026-09-15 15:59:27 +0700

    quick fix2

--- src/recon/tool_installer/subfinder.py
+++ src/recon/tool_installer/subfinder.py
@@ -8,7 +8,7 @@ arch = platform.machine()
 
 
 def install(dest):
-    if system != "windows":
+    if str(system).lower() != "windows":
         print("Install for windows only, failing to install subfinder will only prevent the usage of -d flag")
         return
 
